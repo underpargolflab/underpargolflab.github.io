d009a2c26ca49a9998a84a1b4d3f477d57a7ca1d
Richard Lin <richardlin4375@gmail.com>
2023-11-21 22:07:16 -0500

    Services section and other fixes before pricing section

--- index.js
+++ index.js
@@ -12,3 +12,49 @@ document.getElementById("services-trackman").onmouseleave = function() {
     document.getElementById("services-trackman-shown").style="opacity: 0;";
     document.getElementById("services-trackman-main").style="opacity: 1;";
 };
+
+document.getElementById("services-fitness").onmouseover = function() {
+    document.getElementById("services-fitness-img").style = "filter: brightness(10%);";
+    document.getElementById("services-fitness").style = "cursor: pointer;";
+    document.getElementById("services-fitness-shown").style="opacity: 1;";
+    document.getElementById("services-fitness-main").style="opacity: 0;";
+
+
+};
+document.getElementById("services-fitness").onmouseleave = function() {
+    document.getElementById("services-fitness-img").style = "filter: brightness(40%);";
+    document.getElementById("services-fitness").style = "cursor: default;";
+    document.getElementById("services-fitness-shown").style="opacity: 0;";
+    document.getElementById("services-fitness-main").style="opacity: 1;";
+};
+
+document.getElementById("services-puttview").onmouseover = function() {
+    document.getElementById("services-puttview-img").style = "filter: brightness(10%);";
+    document.getElementById("services-puttview").style = "cursor: pointer;";
+    document.getElementById("services-puttview-shown").style="opacity: 1;";
+    document.getElementById("services-puttview-main").style="opacity: 0;";
+
+
+};
+document.getElementById("services-puttview").onmouseleave = function() {
+    document.getElementById("services-puttview-img").style = "filter: brightness(40%);";
+    document.getElementById("services-puttview").style = "cursor: default;";
+    document.getElementById("services-puttview-shown").style="opacity: 0;";
+    document.getElementById("services-puttview-main").style="opacity: 1;";
+};
+
+document.getElementById("services-social").onmouseover = function() {
+    document.getElementById("services-social-img").style = "filter: brightness(10%);";
+    document.getElementById("services-social").style = "cursor: pointer;";
+    document.getElementById("services-social-shown").style="opacity: 1;";
+    document.getElementById("services-social-main").style="opacity: 0;";
+
+
+};
+document.getElementById("services-social").onmouseleave = function() {
+    document.getElementById("services-social-img").style = "filter: brightness(40%);";
+    document.getElementById("services-social").style = "cursor: default;";
+    document.getElementById("services-social-shown").style="opacity: 0;";
+    document.getElementById("services-social-main").style="opacity: 1;";
+};
+
--- universal.js
+++ universal.js
@@ -1,4 +1,22 @@
 let width = window.screen.width;
+var offset;
+if (width >= 1024) {
+    offset = 125;
+}
+else {
+    offset=300;
+}
+document.getElementById("menuimage").onmousedown = function() {
+    scrollTo({top: document.getElementById("landing").offsetTop - offset, behavior: "smooth"});
+};
+document.getElementById("homeb").onmousedown = function() {
+    scrollTo({top: document.getElementById("landing").offsetTop, behavior: "smooth"});
+};
+document.getElementById("servicesb").onmousedown = function() {
+    scrollTo({top: document.getElementById("services").offsetTop - offset, behavior: "smooth"});
+};
+
+
 if (width > 1024) {
     setInterval(function () {
         if (window.scrollY <= 100) {
@@ -7,9 +25,9 @@ if (width > 1024) {
             document.getElementById("menuimage").src="./images/logo.png";
         }
         else {
-            document.getElementById("menu").style = "background-color: white; border-bottom: 3px solid var(--main); ";
-            const menuLinks = document.querySelectorAll('.menu-link'); menuLinks.forEach(link => { link.style.color = 'var(--dark)' }); 
-            document.getElementById("menuimage").src="./images/logo-recolor.png";
+            document.getElementById("menu").style = "background-color: var(--dark); border-bottom: 3px solid var(--main); ";
+            const menuLinks = document.querySelectorAll('.menu-link'); menuLinks.forEach(link => { link.style.color = 'white' }); 
+            document.getElementById("menuimage").src="./images/logo.png";
         }
     }, 10)
 }
