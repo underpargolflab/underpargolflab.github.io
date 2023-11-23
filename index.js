81d7f7b8434969862ecd1d43d9adaa7c0c0a2f1b
Richard Lin <richardlin4375@gmail.com>
2023-11-23 17:02:39 -0500

    Services and Pricing Sections finished

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ document.getElementById("services-trackman").onmouseover = function() {
 
 };
 document.getElementById("services-trackman").onmouseleave = function() {
-    document.getElementById("services-trackman-img").style = "filter: brightness(40%);";
+    document.getElementById("services-trackman-img").style = "filter: brightness(65%);";
     document.getElementById("services-trackman").style = "cursor: default;";
     document.getElementById("services-trackman-shown").style="opacity: 0;";
     document.getElementById("services-trackman-main").style="opacity: 1;";
@@ -22,7 +22,7 @@ document.getElementById("services-fitness").onmouseover = function() {
 
 };
 document.getElementById("services-fitness").onmouseleave = function() {
-    document.getElementById("services-fitness-img").style = "filter: brightness(40%);";
+    document.getElementById("services-fitness-img").style = "filter: brightness(65%);";
     document.getElementById("services-fitness").style = "cursor: default;";
     document.getElementById("services-fitness-shown").style="opacity: 0;";
     document.getElementById("services-fitness-main").style="opacity: 1;";
@@ -37,7 +37,7 @@ document.getElementById("services-puttview").onmouseover = function() {
 
 };
 document.getElementById("services-puttview").onmouseleave = function() {
-    document.getElementById("services-puttview-img").style = "filter: brightness(40%);";
+    document.getElementById("services-puttview-img").style = "filter: brightness(65%);";
     document.getElementById("services-puttview").style = "cursor: default;";
     document.getElementById("services-puttview-shown").style="opacity: 0;";
     document.getElementById("services-puttview-main").style="opacity: 1;";
@@ -52,7 +52,7 @@ document.getElementById("services-social").onmouseover = function() {
 
 };
 document.getElementById("services-social").onmouseleave = function() {
-    document.getElementById("services-social-img").style = "filter: brightness(40%);";
+    document.getElementById("services-social-img").style = "filter: brightness(65%);";
     document.getElementById("services-social").style = "cursor: default;";
     document.getElementById("services-social-shown").style="opacity: 0;";
     document.getElementById("services-social-main").style="opacity: 1;";
